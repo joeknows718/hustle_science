5f886cfb958e73271b52531716673e15b32cbbfb
Joe C <joeknows718@Joes-MacBook-Pro.local>
2015-06-14 15:02:21 -0400

    current views mapped in hs_pkg

--- hustle_science_pkg/urls.py
+++ hustle_science_pkg/urls.py
@@ -3,4 +3,11 @@ from hustle_science_pkg import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
+	url(r'^posts-business/', views.post_business, name='/posts-business' ),
+	url(r'^posts-technology/', views.post_tech, name='/posts-technology' ),
+	url(r'^posts-fashion/', views.post_fashion, name='/posts-fashion' ),
+	url(r'^posts-music/', views.post_music, name='/posts-music' ),
+	url(r'^posts-art/', views.post_art, name='/posts-art' ),
+	url(r'^posts-news/', views.post_news, name='/posts-news' ),
+	url(r'^posts-culture/', views.post_culture, name='/posts-culture' ),
 	)
